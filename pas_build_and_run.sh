1b155a3566433b0d4150bc34a95de8e4b4d3a350
pepfof <artemiodro@gmail.com>
2020-10-19 14:26:16 +0300

    Build script update

--- pas_build_and_run.sh
+++ pas_build_and_run.sh
@@ -1,2 +1,2 @@
-g++ -rdynamic `pkg-config --cflags gtk+-3.0 glib-2.0` -o pas pas.c `pkg-config --libs gtk+-3.0 glib-2.0` 
-./pas
+g++ -rdynamic `pkg-config --cflags gtk+-3.0 glib-2.0` -o pas.o pas.c `pkg-config --libs gtk+-3.0 glib-2.0` 
+./pas.o
